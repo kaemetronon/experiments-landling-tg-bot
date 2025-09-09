486d42db52f70d08a85d898f93c1fec7e0722f9f
kametronon <e@mail>
2025-09-09 23:54:13 +0300

    metrika

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { GeistSans } from "geist/font/sans"
 import { GeistMono } from "geist/font/mono"
 import { Space_Grotesk, DM_Sans } from "next/font/google"
 import { Suspense } from "react"
+import Script from "next/script"
 import "./globals.css"
 import ThemeToggle from "@/components/ThemeToggle"
 
@@ -31,7 +32,7 @@ export const metadata: Metadata = {
       ],
       apple: "/apple-touch-icon.png",
     },
-    manifest: "/site.webmanifest"
+    manifest: "/site.webmanifest",
 }
 
 export default function RootLayout({
@@ -41,9 +42,36 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
+      <head>
+        <Script
+          id="yandex-metrika"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=104090102', 'ym');
+
+              ym(104090102, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+            `,
+          }}
+        />
+      </head>
       <body
         className={`font-sans ${GeistSans.variable} ${GeistMono.variable} ${spaceGrotesk.variable} ${dmSans.variable}`}
       >
+        <noscript>
+          <div>
+            <img 
+              src="https://mc.yandex.ru/watch/104090102" 
+              style={{position: 'absolute', left: '-9999px'}} 
+              alt="" 
+            />
+          </div>
+        </noscript>
         <div className="fixed right-4 top-4 z-50">
           <ThemeToggle />
         </div>
